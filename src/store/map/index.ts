d78c789d869ef3d3d640e6f29f30e03b81c3c3e5
Marcus Peterson <mpeterson@atlassian.com>
2018-10-03 21:07:50 +1000

    Tests for Map Store

--- src/store/map/index.ts
+++ src/store/map/index.ts
@@ -4,7 +4,7 @@ export const createMapStore = <K, V>(): IStore<K, V> => {
   const map = new Map<K, V>();
   return {
     delete: (key: K): boolean => map.delete(key),
-    get: (key: K): V => map.get(key),
+    get: (key: K): V => map.get(key) || null,
     put: (key: K, value: V): void => {
       map.set(key, value);
     },
---
+++ src/store/map/test.ts
@@ -0,0 +1,56 @@
+import { createMapStore } from ".";
+import { IStore } from "../types";
+
+type Key = string;
+type Value = { data: boolean };
+
+describe("Map Store", () => {
+  describe("get", () => {
+    test("should return null if not present", () => {
+      const store: IStore<Key, Value> = createMapStore();
+      expect(store.get("value")).toBeNull();
+    });
+    test("should return value if present", () => {
+      const store: IStore<Key, Value> = createMapStore();
+      const value = { data: true };
+      store.put("value", value);
+      expect(store.get("value")).toBe(value);
+      expect(store.get("value").data).toBe(true);
+    });
+  });
+
+  describe("put", () => {
+    test("should insert the object into the store", () => {
+      const store: IStore<Key, Value> = createMapStore();
+      expect(store.get("value")).toBeNull();
+      const value = { data: true };
+      store.put("value", value);
+      expect(store.get("value")).toBe(value);
+      expect(store.get("value").data).toBe(true);
+    });
+    test("should overwrite previous values", () => {
+      const store: IStore<Key, Value> = createMapStore();
+      const value = { data: true };
+      const newValue = { data: true };
+
+      store.put("value", value);
+      expect(store.get("value")).toBe(value);
+
+      store.put("value", newValue);
+      expect(store.get("value")).toBe(newValue);
+    });
+  });
+
+  describe("delete", () => {
+    test("should remove the item from the store", () => {
+      const store: IStore<Key, Value> = createMapStore();
+      const value = { data: true };
+
+      store.put("value", value);
+      expect(store.get("value")).toBe(value);
+
+      store.delete("value");
+      expect(store.get("value")).toBeNull();
+    });
+  });
+});
